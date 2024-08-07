e4552dce43a65f413a185627c15d0b7b33a08a05
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-08-07 16:42:38 +0200

    remove redux react router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+
 import Home from "../Home/Home";
-import NotFound from "../NotFound/NotFound";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
