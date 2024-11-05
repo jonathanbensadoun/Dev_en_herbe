a8cfeadef32d4d4fcf645db1f53147557e4099ab
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-11-05 17:48:26 +0100

    create page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-purple-950 text-white flex justify-center items-center h-screen">
-      <p className="text-6xl">Template React Redux Tailwind Vite</p>
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-900">
+      <img
+        src="/src/assets/rocket.png"
+        alt="Placeholder Image"
+        className="w-1/2 md:w-1/4 rounded-lg"
+      />
+      <button
+        className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4"
+        onClick={() => (window.location.href = "https://discord.gg/ckNy9GeK")}
+      >
+        Join Discord
+      </button>
     </div>
   );
 };
